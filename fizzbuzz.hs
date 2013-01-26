1464a6bb15a54767381699d5fc40a0751b0363d2
Tomohiko Himura <eiel.hal@gmail.com>
2013-01-26 23:39:32 +0900

    余剰を使わない FizzBuzz かいてみた

--- fizzbuzz.hs
+++ fizzbuzz.hs
@@ -12,3 +12,19 @@ fizzbuzz n | n `mod` 15 == 0 = "FizzBuzz"
            | n `mod`  5 == 0 = "Buzz"
            | n `mod`  3 == 0 = "Fizz"
            | otherwise       = show n
+
+-- 余剰演算を利用しない FizzBuzzを作れって @sugarsupot にいわれたので
+data Fizzbuzz = Number | Fizz | Buzz | FizzBuzz
+
+-- print $ take 100 $ fizzbuzz2
+fizzbuzz2 :: [String]
+fizzbuzz2 = map logic $ zip [1..] source
+  where
+    source = cycle [Number, Number, Fizz, Number, Buzz,
+                    Fizz, Number, Number, Fizz, Buzz,
+                    Number, Fizz, Number, Number, FizzBuzz]
+    logic :: (Int, Fizzbuzz) -> String
+    logic (_, Fizz)     = "Fizz"
+    logic (_, Buzz)     = "Buzz"
+    logic (_, FizzBuzz) = "FizzBuzz"
+    logic (n, _)        = show n
