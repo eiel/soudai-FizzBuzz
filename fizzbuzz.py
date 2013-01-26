a99003474f2fd078211b4e4e144b9e792ce3d8e5
Tomohiko Himura <eiel.hal@gmail.com>
2013-01-26 14:31:35 +0900

    普通ならこうかくだろ…

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,24 +1,17 @@
-int = int(raw_input("Please enter an integer: "))
+# coding: utf-8 #
 
-def do_fizz(int):
-    if (int % 3) == 0:
-        return 1
-    return 0
+def fizzbuzz(number):
+    if number % 15 == 0:  # number % 5 == 0 and number % 3 == 0
+        return "FizzBuzz"
+    elif number % 5 == 0:
+        return "Buzz"
+    elif number % 3 == 0:
+        return "Fizz"
+    else:
+        return str(number)
 
-def do_buzz(int):
-    if (int % 5) == 0:
-        return 2
-    return 0
-
-def do_answer(fizz, buzz):
-    flag = fizz + buzz
-    if flag == 0:
-        print int #引数に居なくても外のintを参照出来る
-    elif flag == 1:
-        print "Fizz"
-    elif flag == 2:
-        print "Buzz"
-    elif flag == 3:
-        print "FizzBuzz"
-
-do_answer(do_fizz(int), do_buzz(int))
+if __name__ == '__main__':
+    number = int(raw_input("Please enter an integer: "))
+    print fizzbuzz(number)
+    # for n in range(1,100):
+    #     print fizzbuzz(n)
